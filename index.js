d1ccfff293023cd7eeed66d47214c292581129bf
Choi Jiwon <winning1993@naver.com>
2020-05-12 02:57:42 +0900

    슬라이더 에니메이션 완성

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const slideBtn = document.getElementById("slideBtn");
-const slider = document.getElementById("slider")
+const slider = document.getElementById("slider");
+const mainArea = document.getElementById("mainArea");
 
 function handleSlide(){
     console.log("xxxx")
-    slider.style.animation = "showSlider 0.5s ease-in-out forwards";
+    slider.style.animation = "showSlider 0.3s ease-in-out forwards";
+    mainArea.style.animation = "showSlider 0.3s ease-in-out forwards";
+    slideBtn.removeEventListener("click", handleSlide);
+    slideBtn.addEventListener("click", closeSlide)
+}
+
+function closeSlide(){
+    slider.style.animation = "closeSlider 0.3s ease-in-out forwards";
+    mainArea.style.animation = "closeSlider 0.3s ease-in-out forwards";
+    slideBtn.removeEventListener("click", closeSlide);
+    slideBtn.addEventListener("click", handleSlide)
 }
 
 function init(){
