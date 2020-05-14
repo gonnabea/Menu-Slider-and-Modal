e93078ca0a92894b294407d8babccd077aa73157
Choi Jiwon <winning1993@naver.com>
2020-05-15 05:26:57 +0900

    모달창, 기본 페이지

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const slideBtn = document.getElementById("slideBtn");
 const slider = document.getElementById("slider");
-const mainArea = document.getElementById("mainArea");
+const mainArea = document.getElementById("mainArea"),
+signUpBtn = document.getElementById("signUpBtn"),
+signUpModal = document.getElementById("signUpModal"),
+modalCloseBtn = document.getElementById("modalCloseBtn")
 
 function handleSlide(){
     console.log("xxxx")
     slider.style.animation = "showSlider 0.3s ease-in-out forwards";
     mainArea.style.animation = "showSlider 0.3s ease-in-out forwards";
+    slideBtn.style.animation = "btnmove 0.3s ease-in-out forwards";
     slideBtn.removeEventListener("click", handleSlide);
     slideBtn.addEventListener("click", closeSlide)
 }
@@ -13,12 +17,25 @@ function handleSlide(){
 function closeSlide(){
     slider.style.animation = "closeSlider 0.3s ease-in-out forwards";
     mainArea.style.animation = "closeSlider 0.3s ease-in-out forwards";
+    slideBtn.style.animation = "revertBtn 0.3s ease-in-out forwards";
     slideBtn.removeEventListener("click", closeSlide);
     slideBtn.addEventListener("click", handleSlide)
 }
 
+function handleSignUp(){
+    mainArea.style.backgroundColor = "rgba(0,0,0,0.7)";
+    signUpModal.style.display = "block";
+}
+
+function modalClose(){
+    signUpModal.style.display = "none";
+    mainArea.style.backgroundColor = "white";
+}
+
 function init(){
     slideBtn.addEventListener("click", handleSlide);
+    signUpBtn.addEventListener("click", handleSignUp);
+    modalCloseBtn.addEventListener("click", modalClose);
 }
 
 init()
