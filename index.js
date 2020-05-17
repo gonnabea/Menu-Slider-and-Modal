23c8abfe135204e38a8bd07179c0f91bc548c00e
Choi Jiwon <winning1993@naver.com>
2020-05-17 19:23:24 +0900

    반응형 (완성)

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function handleSlide(){
     slider.style.animation = "showSlider 0.3s ease-in-out forwards";
     mainArea.style.animation = "showSlider 0.3s ease-in-out forwards";
     slideBtn.style.animation = "btnmove 0.3s ease-in-out forwards";
+
+    if(window.matchMedia("(max-width: 450px)").matches){
+        mainArea.style.animation = "showSliderMob 0.3s ease-in-out forwards";
+    }
+
     slideBtn.removeEventListener("click", handleSlide);
     slideBtn.addEventListener("click", closeSlide)
 }
@@ -19,6 +24,11 @@ function closeSlide(){
     slider.style.animation = "closeSlider 0.3s ease-in-out forwards";
     mainArea.style.animation = "closeSlider 0.3s ease-in-out forwards";
     slideBtn.style.animation = "revertBtn 0.3s ease-in-out forwards";
+
+    if(window.matchMedia("(max-width: 450px)").matches){
+        mainArea.style.animation = "closeSliderMob 0.3s ease-in-out forwards";
+    }
+
     slideBtn.removeEventListener("click", closeSlide);
     slideBtn.addEventListener("click", handleSlide)
 }
